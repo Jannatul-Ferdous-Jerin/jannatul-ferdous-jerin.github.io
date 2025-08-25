fd1fb48cc4f6d9ba6fe081dabe8500f6330b0369
Jannatul Ferdous Jerin <ferdousjerin261@gmail.com>
2025-08-25 12:27:18 +0600

    update

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,2 @@
-convert -background transparent "images/rabeya-akter.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+magick -background transparent "images/jannatul-ferdous.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+
